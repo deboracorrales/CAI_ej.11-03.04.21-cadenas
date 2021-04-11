10cb64dae81b7a8fd9d5ea0634459a5d20176ec5
deboracorrales <deboramailen88@gmail.com>
2021-04-11 03:45:17 -0300

    Agregar archivos de proyecto.

---
+++ CAI_ej.11-03.04.21-cadenas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_ej._11_03._04._21_cadenas
+//Ej 11: Reemplace en una cadena ingresada por el usuario todas 
+//las vocales acentuadas por vocales sin acento y presente el resultado.	
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            string palabraConTildes = "áéíóúñ";
+            //vocales = ["áéíóúñ"];
+            Console.WriteLine("Ingrese una frase.");
+            texto = Console.ReadLine();
+            Console.WriteLine(texto.Replace("á", "a").Replace("é", "e").Replace("í", "i").Replace("ó", "o").Replace("ú", "u"));
+
+            Console.ReadKey();
+        }
+    }
+}
+  
